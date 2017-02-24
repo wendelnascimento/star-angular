46803f6fd1bcee59f8141d7d095ee88e91fe7449
Wendel Nascimento <wendelnascimento@MBP-de-Wendel.workgroup>
2017-02-24 15:25:51 -0300

    new gh-pages version

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes, RouterModule } from '@angular/router';
-import { PeopleComponent } from './people/people.component';
-
-export const ROUTES: Routes = [
-    {
-        path: '',
-        component: PeopleComponent
-    }
-]
